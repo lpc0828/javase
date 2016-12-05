b0155e0d6b656e03f2a54cdcc2378812f6df3c08
lpc0828 <pengcheng.liu0828@gmail.com>
2016-12-06 07:20:46 +0800

    多线程测试

---
+++ src/main/java/com/pengcheng/multithread/ThreadTest1.java
@@ -0,0 +1,37 @@
+package com.pengcheng.multithread;
+
+import sun.misc.Contended;
+import sun.misc.VM;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @version V1.0.0
+ * @author: <a href="mailto:pengcheng.liu0828@gmail.com">pc.liu</a>.
+ * @since: 2016/12/6.
+ */
+public class ThreadTest1 {
+
+    public static void main(String[] args) {
+        MyThread1 t1 = new MyThread1();
+        t1.start();
+    }
+}
+
+class MyThread1 implements Runnable {
+
+    private static native void registerNatives();
+    static {
+        registerNatives();
+    }
+
+    public void run() {
+        System.out.println(" execute MyThread1");
+    }
+
+    private native void start0();
+
+    public void start() {
+        start0();
+    }
+}
